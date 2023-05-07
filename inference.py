e78b9f36f434ec3e515762537d053d484df3a3bb
BCWhiteStar <86469098+BCWhiteStar@users.noreply.github.com>
2023-05-07 14:21:26 +0800

    Add files via upload

---
+++ inference.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+from torch.utils.tensorboard import SummaryWriter
+import torchaudio
+
+writer = SummaryWriter("results/")
+spk = input("Speaker:")
+key = input("Key:")
+for model in os.listdir("logs/44k"):
+    if "G_" in model and model != "G_0.pth":
+        model_itera = model.split('_')[1].split('.')[0]
+        str = "python inference_main.py -m \"" + os.path.join("logs/44k", model) + "\"" \
+                + " -c \"configs/config.json\" -n \"富士山下.wav\" -t "+ key +" -s \"" \
+                + spk + "\" -fmp"
+        print(str)
+        os.system(str)
+
+for wav in os.listdir("results"):
+    if ".wav" in wav:
+        audio, _ = torchaudio.load(os.path.join("results", wav))
+        writer.add_audio("iter:{}".format(wav.split('G')[1].split('_')[1]),audio, 0)
